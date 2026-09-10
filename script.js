d712cb121a84f9915f902e4673d31fb8b6849bd6
wong ndeso <107795971+ikhwanmunajat@users.noreply.github.com>
2026-09-10 22:58:16 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("year").textContent = new Date().getFullYear();
+
+window.addEventListener("scroll",()=>{
+document.querySelector("header").style.boxShadow="0 5px 25px #000";
+});

a8a59f0eb4366bc99936c00cdcbf19740cc01fba
wong ndeso <107795971+ikhwanmunajat@users.noreply.github.com>
2026-09-10 23:11:29 +0700

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("year").textContent = new Date().getFullYear();
-
-window.addEventListener("scroll",()=>{
-document.querySelector("header").style.boxShadow="0 5px 25px #000";
-});
